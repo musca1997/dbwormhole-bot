9940748dcbc4717f428c84bd6b34e6278f1a82c0
Musca <musturtwig@gmail.com>
2018-10-30 23:19:31 +0800

    added douban_thing

--- wormholebot.py
+++ wormholebot.py
@@ -8,6 +8,36 @@ import random
 email=  'PUT EMAIL HERE'
 dou_password = 'PUT PASS HERE'
 
+def get_thing():
+    i = random.randint(1000001, 30333333)
+    douban_url = 'https://www.douban.com/thing/' + str(i)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'q=0.8,en-us;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'DNT': '1',
+            'Connection': 'keep-alive'}
+
+    r = requests.get(douban_url, headers=headers)
+    if not (r.status_code == 200):
+        return
+    soup = BeautifulSoup(r.content,"html.parser")
+    group_title = soup.title.string
+    group_description = soup.find('div', attrs={"class": "intro"})
+
+    text = ''
+    if group_description is not None:
+        group_description = group_description.get_text()
+        for i in group_description:
+            text = text + i
+    group_data = group_title + '\n' + douban_url + '\n' + text
+    group_data = group_data.replace('https://', '')
+    print group_data
+    app = doubanrobot.DoubanRobot(email, dou_password)
+    app.talk_status(group_data)
+    time.sleep(3600)
+    get_group()
+
 def get_group():
     i = random.randint(10001, 640099)
     douban_url = 'https://www.douban.com/group/'+ str(i)
@@ -100,4 +130,4 @@ def get_list():
 
 if __name__ == '__main__':
     while True:
-        get_group()
+        get_thing()
